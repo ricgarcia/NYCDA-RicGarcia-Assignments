f31afc764cf11639710cd34153b53df0fb413394
Ric Garcia <ricgarcia.me@gmail.com>
2017-03-25 01:47:19 -0400

    Got the order of the resets fixed

--- RicGarciaCookieConfectionary/cookiesconfectionary.js
+++ RicGarciaCookieConfectionary/cookiesconfectionary.js
@@ -13,20 +13,6 @@ $(document).ready(function() {
     $('#sugar-count').html(new_sugar_count)
   })
 
-  var current_chocolate_count = Cookies.get('chocolate-key')
-  $('#chocolate-count').html(current_chocolate_count)
-
-  $('#chocolate').click(function() {
-    var current_chocolate_count = parseInt(Cookies.get('chocolate-key'))
-
-    if (!current_chocolate_count) {
-      current_chocolate_count = 0
-    }
-    var new_chocolate_count = current_chocolate_count + 1
-    Cookies.set('chocolate-key', new_chocolate_count)
-    $('#chocolate-count').html(new_chocolate_count)
-  })
-
   var current_lemon_count = Cookies.get('lemon-key')
   $('#lemon-count').html(current_lemon_count)
 
@@ -41,17 +27,30 @@ $(document).ready(function() {
     $('#lemon-count').html(new_lemon_count)
   })
 
+  var current_chocolate_count = Cookies.get('chocolate-key')
+  $('#chocolate-count').html(current_chocolate_count)
+
+  $('#chocolate').click(function() {
+    var current_chocolate_count = parseInt(Cookies.get('chocolate-key'))
+
+    if (!current_chocolate_count) {
+      current_chocolate_count = 0
+    }
+    var new_chocolate_count = current_chocolate_count + 1
+    Cookies.set('chocolate-key', new_chocolate_count)
+    $('#chocolate-count').html(new_chocolate_count)
+  })
 
 
   $('#reset').click(function() {
     Cookies.set('sugar-key', 0)
     $('#sugar-count').html(0)
   })
-  $('#reset3').click(function() {
+  $('#reset2').click(function() {
   Cookies.set('lemon-key', 0)
   $('#lemon-count').html(0)
   })
-  $('#reset2').click(function() {
+  $('#reset3').click(function() {
   Cookies.set('chocolate-key', 0)
   $('#chocolate-count').html(0)
   })
