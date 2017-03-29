a6935c66fc7152eec30ace9c9c01124a53ecf1a6
Ric Garcia <ricardo21579@gmail.com>
2017-03-29 01:22:15 -0400

    added jquery to product page

--- RicGarciaFinalProject/main.js
+++ RicGarciaFinalProject/main.js
@@ -1,40 +1,31 @@
 $(document).ready(function() {
     if (Cookies.get('congrats')) {
       alert('Today only, get 10% off subscription!')
+      $(Cookies.get('congrats'))
     }
       $('#submit').click(function() {
         var signup = $('#somename').val()
           Cookies.set('congrats', signup)
           alert('Congratulations ' + signup + '! Welcome to our club!')
     })
-
-
-/* Modal JS*/
-// Get the modal
-var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
-
+    $("#proddscpt1").hide();
+    $("#prod1 button").click(function(){
+        $("#proddscpt1").toggle();
+    });
+    $("#proddscpt2").hide();
+    $("#prod2 button").click(function(){
+        $("#proddscpt2").toggle();
+    });
+    $("#proddscpt3").hide();
+    $("#prod3 button").click(function(){
+        $("#proddscpt3").toggle();
+    });
+    $("#proddscpt4").hide();
+    $("#prod4 button").click(function(){
+        $("#proddscpt4").toggle();
+    });
+    $("#proddscpt5").hide();
+    $("#prod5 button").click(function(){
+        $("#proddscpt5").toggle();
+    });
   })
